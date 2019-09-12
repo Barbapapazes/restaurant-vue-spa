cbe204525559556d69faa4906f555f7b3c1bfc2f
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-09-12 08:49:23 +0200

    remove dist

--- dist/service-worker.js
+++
@@ -1,4 +0,0 @@
-importScripts("/restaurant-vue-spa/precache-manifest.48df56f83cb3d798080343389964a2b0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest)
-
